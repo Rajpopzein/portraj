729af2d2ab61699b8f1eed160b634829ae324f9e
Rajkumar <46551565+Rajpopzein@users.noreply.github.com>
2019-03-28 19:51:27 +0530

    update

--- js/main.js
+++ js/main.js
@@ -1,37 +1,38 @@
-$(document).ready(function(){
-    var typed = new Typed('#typed',{
-        strings: ["Hello There","I am Rajkumar","A UI / Web Developer","A Photographer"],
-        typeSpeed:40,
-        backSpeed:40,
-        backDelay:1000,
-        loop:true
+$(document).ready(function () {
+    var typed = new Typed('#typed', {
+        strings: ["Hello There", "I am Rajkumar", "A UI / Web Developer", "A Photographer"],
+        typeSpeed: 40,
+        backSpeed: 40,
+        backDelay: 1000,
+        loop: true
     });
 
-    $('.menu').click(function(){
+    $('.menu').click(function () {
         $('.navbar nav ul li').toggleClass('active');
     });
 
-    $(window).on('scroll',function(){
-        if($(window).scrollTop()){
+    $(window).on('scroll', function () {
+        if ($(window).scrollTop()) {
             $('.nbr').addClass('black');
             $('.logo').addClass('logo_black');
             $('.navbar nav ul li a').addClass('nav-item_black');
-        }else{
+            $('.toggler').addClass('logo_black')
+        } else {
             $('.nbr').removeClass('black');
             $('.logo').removeClass('logo_black');
             $('.navbar nav ul li a').removeClass('nav-item_black');
+            $('.toggler').removeClass('logo_black');
         }
     })
     //scroll 
     var scrollClick = $('.nav-item');
-    scrollClick.click(function(e){
+    scrollClick.click(function (e) {
         e.preventDefault();
         $('body,html').animate({
-            scrollTop:
-            $(this.hash).offset().top
-        },1500);
+            scrollTop: $(this.hash).offset().top
+        }, 1500);
     });
     //link switch
     //hash is resprsending id
-    
+
 });
